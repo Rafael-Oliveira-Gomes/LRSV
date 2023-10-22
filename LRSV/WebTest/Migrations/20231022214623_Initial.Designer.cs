cba0b0653f54e8d5e5e9487ae8548d03ef27d2d7
Rafael de Oliveira <rafael.gomes@aec.com.br>
2023-10-22 20:02:16 -0300

    Férias para o colaborador, descontos de INSS, etc

--- LRSV/WebTest/Migrations/20231022214623_Initial.Designer.cs
+++ LRSV/WebTest/Migrations/20231022214623_Initial.Designer.cs
@@ -9,7 +9,7 @@ using WebTest.Model;
 namespace WebTest.Migrations
 {
     [DbContext(typeof(AuthDbContext))]
-    [Migration("20231022175745_Initial")]
+    [Migration("20231022214623_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
--- LRSV/WebTest/Pages/Pagamento.cshtml.cs
+++ LRSV/WebTest/Pages/Pagamento.cshtml.cs
@@ -1,8 +1,7 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using WebTest.Model;
 using Microsoft.AspNetCore.Identity;
-using System.Globalization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
+using WebTest.Model;
 
 namespace WebTest.Pages
 {
@@ -17,14 +16,27 @@ namespace WebTest.Pages
             _userManager = userManager;
         }
 
-        public decimal Salario { get; set; }
-        public string Nome { get;set; }
+        public double SalarioBruto { get; set; }
+        public double SalarioLiquido { get; set; }
+        public string Nome { get; set; }
+        public double inss { get; set; }
 
         public void OnGet()
         {
             var user = _userManager.GetUserAsync(User).Result;
             Nome = user.Nome;
-            Salario = user.Salario;
+
+            var ultimoPagamento = _context.Pagamentos
+                .Where(p => p.NomeUsuario == Nome)
+                .OrderByDescending(p => p.Id)
+                .FirstOrDefault();
+
+            if (ultimoPagamento != null)
+            {
+                SalarioBruto = ultimoPagamento.SalarioBruto;
+                SalarioLiquido = ultimoPagamento.SalarioLiquido * 0.9;
+                inss = SalarioBruto - SalarioLiquido;
+            }
         }
     }
 }
